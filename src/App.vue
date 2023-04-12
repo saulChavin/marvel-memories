2525e1b31bf2e1fe7f195837ebad44d4d4eadf0e
saulChavin <sagus_97@hotmail.com>
2023-04-12 00:48:19 -0600

    clip path divs (just some trials)

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,25 @@ const difficultyStore = useSettingsStore();
   <header class="w-full">
     <HeaderMenu />
   </header>
-  <main class="container mx-auto relative">
-    <div v-if="!difficultyStore.difficulty" class="flex flex-col gap-4 max-w-xs px-2 mx-auto font-semibold">
-      <button @click="difficultyStore.setDifficulty('easy')"
-        class="bg-cyan-700 rounded-md py-1 hover:bg-cyan-600">Fácil</button>
-      <button @click="difficultyStore.setDifficulty('medium')"
-        class="bg-orange-400 rounded-md py-1 hover:bg-amber-600">Intermedio</button>
-      <button @click="difficultyStore.setDifficulty('hard')"
-        class="bg-red-500 rounded-md py-1 hover:bg-red-600">Dificil</button>
+  <main class="container mx-auto relative h-fit flex flex-col gap-1">
+    <div v-if="!difficultyStore.difficulty" class="flex gap-1 px-2 mx-auto justify-center font-semibold w-full h-full">
+      <div class="easy flex-1 h-52 w-52 bg-orange-600">
+        Facil
+      </div>
+      <div class="medium flex-1 h-52 w-52 bg-slate-700">
+        Intermedio
+      </div>
+      <div class="hard flex-1 h-52 w-52 bg-fuchsia-500">
+        Dificil
+      </div>
+    </div>
+    <div v-if="!difficultyStore.difficulty" class="flex gap-1 px-2 mx-auto justify-center font-semibold w-full h-full">
+      <div class="flex-1 another h-52 w-52 bg-sky-700 fle">
+        Intermedio
+      </div>
+      <div class="another2 h-52 w-52 bg-rose-500 flex-1">
+        Dificil
+      </div>
     </div>
 
     <div v-if="difficultyStore.difficulty">
@@ -29,10 +40,42 @@ const difficultyStore = useSettingsStore();
 </template>
 
 <style>
+.easy {
+  clip-path: polygon(0% 0%, 80% 0%, 100% 90%, 0% 100%);
+}
+
+.medium {
+  margin-left: -6%;
+  clip-path: polygon(0 0, 100% 0%, 80% 85%, 20% 90%);
+}
+
+.hard {
+  margin-left: -6%;
+  clip-path: polygon(20% 0, 100% 0%, 100% 100%, 0% 85%);
+}
+
+.another {
+  /* margin-left: 5%; */
+  margin-top: -4%;
+  clip-path: polygon(0 13%, 100% 0%, 100% 100%, 0 100%);
+}
+
+.another2 {
+  margin-top: -5%;
+  clip-path: polygon(0 4%, 22% 0%, 100% 100%, 0 100%);
+}
+
 body {
+  height: 100vh;
   background-color: #2d2d30;
   margin: 0;
   padding: 0;
   font-family: 'Comic Neue';
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
